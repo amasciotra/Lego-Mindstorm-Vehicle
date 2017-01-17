4f733d2dc523aa88e71e13fd033c83f6f9b188d2
Thomas Christinck <thomas.christinck@mail.mcgill.ca>
2017-01-17 18:39:10 -0500

    Lab 2

---
+++ src/lab2EV3Odometer/OdometryCorrection.java
@@ -0,0 +1,44 @@
+/* 
+ * OdometryCorrection.java
+ */
+package lab2EV3Odometer;
+
+public class OdometryCorrection extends Thread {
+	private static final long CORRECTION_PERIOD = 10;
+	private Odometer odometer;
+	//Useful math constants
+	private static final double TWO_PI = Math.PI * 2;
+	private static final double ONE_QUARTER_PI = Math.PI / 4;
+	// constructor
+	public OdometryCorrection(Odometer odometer) {
+		this.odometer = odometer;
+	}
+
+	// run method (required for Thread)
+	public void run() {
+		long correctionStart, correctionEnd;
+
+		while (true) {
+			correctionStart = System.currentTimeMillis();
+
+			// put your correction code here
+
+			// this ensure the odometry correction occurs only once every period
+			correctionEnd = System.currentTimeMillis();
+			if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
+				try {
+					Thread.sleep(CORRECTION_PERIOD
+							- (correctionEnd - correctionStart));
+				} catch (InterruptedException e) {
+					// there is nothing to be done here because it is not
+					// expected that the odometry correction will be
+					// interrupted by another thread
+				}
+			}
+		}
+	}
+	
+	private double wrapAngle(double rads) {
+		return rads % TWO_PI;
+	}
+}
